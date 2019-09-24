127025bc7e16b097239103960b373e49d80c79ea
Simon Hofmann <simon.hofmann@consol.de>
2019-09-24 14:38:12 +0200

    Updated mouse testcases

--- test/mouse.js
+++ test/mouse.js
@@ -5,83 +5,103 @@ let lastKnownPos, currentPos;
 libnut.setMouseDelay(100);
 
 describe("Mouse", () => {
-  it("Get the initial mouse position.", function() {
-    expect((lastKnownPos = libnut.getMousePos())).toBeTruthy();
-    expect(lastKnownPos.x !== undefined).toBeTruthy();
-    expect(lastKnownPos.y !== undefined).toBeTruthy();
+  describe("movement", () => {
+    it("get the initial mouse position.", function() {
+      expect((lastKnownPos = libnut.getMousePos())).toBeTruthy();
+      expect(lastKnownPos.x !== undefined).toBeTruthy();
+      expect(lastKnownPos.y !== undefined).toBeTruthy();
+    });
+
+    it("move the mouse to valid coordinates", () => {
+      lastKnownPos = libnut.moveMouse(0, 0);
+      expect(libnut.moveMouse(100, 100) === 1).toBeTruthy();
+      currentPos = libnut.getMousePos();
+      expect(currentPos).toEqual({ x: 100, y: 100 });
+    });
+
+    it("should throw on more than three parameters", () => {
+      expect(() => libnut.moveMouse(0, 1, 2, 3)).toThrowError(/Invalid number/);
+    });
+
+    it("should throw on less than two parameters", () => {
+      expect(() => libnut.moveMouse(0)).toThrowError(/Invalid number/);
+
+      expect(libnut.moveMouse(0, 0) === 1).toBeTruthy();
+    });
   });
 
-  it("Move the mouse.", function() {
-    lastKnownPos = libnut.moveMouse(0, 0);
-    expect(libnut.moveMouse(100, 100) === 1).toBeTruthy();
-    currentPos = libnut.getMousePos();
-    expect(currentPos.x === 100).toBeTruthy();
-    expect(currentPos.y === 100).toBeTruthy();
+  describe("smooth movement", () => {
+    it("move the mouse smoothly.", () => {
+      lastKnownPos = libnut.moveMouseSmooth(0, 0);
+      expect(libnut.moveMouseSmooth(100, 100) === 1).toBeTruthy();
+      currentPos = libnut.getMousePos();
+      expect(currentPos).toEqual({ x: 100, y: 100 });
 
-    expect(function() {
-      libnut.moveMouse(0, 1, 2, 3);
-    }).toThrowError(/Invalid number/);
+      expect(() => {
+        libnut.moveMouseSmooth(0, 1, 2, 3);
+      }).toThrowError(/Invalid number/);
 
-    expect(function() {
-      libnut.moveMouse(0);
-    }).toThrowError(/Invalid number/);
+      expect(() => {
+        libnut.moveMouseSmooth(0);
+      }).toThrowError(/Invalid number/);
 
-    expect(libnut.moveMouse(0, 0) === 1).toBeTruthy();
+      expect(libnut.moveMouseSmooth(0, 0) === 1).toBeTruthy();
+    });
   });
 
-  it("Move the mouse smoothly.", function() {
-    lastKnownPos = libnut.moveMouseSmooth(0, 0);
-    expect(libnut.moveMouseSmooth(100, 100) === 1).toBeTruthy();
-    currentPos = libnut.getMousePos();
-    expect(currentPos.x).toEqual(100);
-    expect(currentPos.y).toEqual(100);
+  describe("click", () => {
+    it("left", () => {
+      expect(libnut.mouseClick()).toBeTruthy();
+      expect(libnut.mouseClick("left") === 1).toBeTruthy();
+    });
+    it("middle", () => {
+      expect(libnut.mouseClick("middle") === 1).toBeTruthy();
+    });
+    it("right", () => {
+      expect(libnut.mouseClick("right") === 1).toBeTruthy();
+    });
+    it("doubleclick", () => {
+      expect(libnut.mouseClick("left", true)).toBeTruthy();
+    });
 
-    expect(function() {
-      libnut.moveMouseSmooth(0, 1, 2, 3);
-    }).toThrowError(/Invalid number/);
+    it("should throw on invalid input", () => {
+      expect(() => {
+        libnut.mouseClick("0");
+      }).toThrowError(/Invalid mouse/);
+      expect(() => {
+        libnut.mouseClick("party");
+      }).toThrowError(/Invalid mouse/);
+    });
 
-    expect(function() {
-      libnut.moveMouseSmooth(0);
-    }).toThrowError(/Invalid number/);
-
-    expect(libnut.moveMouseSmooth(0, 0) === 1).toBeTruthy();
+    it("should throw too much parameters", () => {
+      expect(() => {
+        libnut.mouseClick("left", 0, "it");
+      }).toThrowError(/Invalid number/);
+    });
   });
 
-  it("Click the mouse.", function() {
-    expect(libnut.mouseClick()).toBeTruthy();
-    expect(libnut.mouseClick("left") === 1).toBeTruthy();
-    expect(libnut.mouseClick("middle") === 1).toBeTruthy();
-    expect(libnut.mouseClick("right") === 1).toBeTruthy();
+  describe("drag", () => {
+    it("should not throw work", () => {
+      expect(libnut.dragMouse(5, 5) === 1).toBeTruthy();
+    });
 
-    expect(libnut.mouseClick("left", true)).toBeTruthy();
+    it("should throw on too few arguments", () => {
+      expect(function() {
+        libnut.dragMouse(0);
+      }).toThrowError(/Invalid number/);
+    });
 
-    expect(function() {
-      libnut.mouseClick("party");
-    }).toThrowError(/Invalid mouse/);
+    it("should throw on too many arguments", () => {
+      expect(function() {
+        libnut.dragMouse(1, 1, "left", 5);
+      }).toThrowError(/Invalid number/);
+    });
 
-    expect(function() {
-      libnut.mouseClick("0");
-    }).toThrowError(/Invalid mouse/);
-
-    expect(function() {
-      libnut.mouseClick("left", 0, "it");
-    }).toThrowError(/Invalid number/);
-  });
-
-  it("Drag the mouse.", function() {
-    expect(libnut.dragMouse(5, 5) === 1).toBeTruthy();
-
-    expect(function() {
-      libnut.dragMouse(0);
-    }).toThrowError(/Invalid number/);
-
-    expect(function() {
-      libnut.dragMouse(1, 1, "left", 5);
-    }).toThrowError(/Invalid number/);
-
-    expect(function() {
-      libnut.dragMouse(2, 2, "party");
-    }).toThrowError(/Invalid mouse/);
+    it("should throw on invalid button arguments", () => {
+      expect(function() {
+        libnut.dragMouse(2, 2, "party");
+      }).toThrowError(/Invalid mouse/);
+    });
   });
 
   it("Mouse scroll.", function() {
