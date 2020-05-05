d2dd2c20fb9325aedbce0275285fa44be5c35ef7
Simon Hofmann <mail@simon-hofmann.org>
2020-05-05 19:39:28 +0200

    (#8) Removed no longer required tests

--- test/bitmap.js
+++ test/bitmap.js
@@ -28,30 +28,4 @@ describe("Bitmap", () => {
     expect(img.height).toEqual(size);
     expect(img.width).toEqual(size);
   });
-
-  it("Get a bitmap and make sure the colorAt works as expected.", function() {
-    const img = libnut.screen.capture();
-    const hex = img.colorAt(0, 0);
-
-    // t.ok(.it(hex), "colorAt returned valid hex.");
-    expect(hex).toMatch(/^[0-9A-F]{6}$/i);
-
-    const screenSize = libnut.getScreenSize();
-    let width = screenSize.width;
-    let height = screenSize.height;
-
-    // Support for higher density screens.
-    const multi = img.width / width;
-    width = width * multi;
-    height = height * multi;
-    expect(() => img.colorAt(0, height)).toThrowError(/are outside the bitmap/);
-    expect(() => img.colorAt(0, height - 1)).not.toThrow();
-    expect(() => img.colorAt(width, 0)).toThrowError(/are outside the bitmap/);
-    expect(() => img.colorAt(9999999999999, 0)).toThrowError(
-      /are outside the bitmap/
-    );
-    expect(() => img.colorAt(0, 9999999999999)).toThrowError(
-      /are outside the bitmap/
-    );
-  });
 });
