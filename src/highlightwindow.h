607ac75a9fb10a60bb72be27cbd5f47d7399df8c
Simon Hofmann <mail@simon-hofmann.org>
2020-05-05 19:55:20 +0200

    (#8) Added extern C

--- src/highlightwindow.h
+++ src/highlightwindow.h
@@ -2,8 +2,17 @@
 #ifndef HIGHLIGHT_WINDOW_H
 #define HIGHLIGHT_WINDOW_H
 
+#ifdef __cplusplus
+extern "C" 
+{
+#endif
+
 #include <stdint.h>
 
 void showHighlightWindow(int32_t x, int32_t y, int32_t width, int32_t height, long duration, float opacity);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
