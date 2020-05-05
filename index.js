56101c3d3ab78ae179b96d249975419f4eaf76a3
Simon Hofmann <mail@simon-hofmann.org>
2020-05-05 19:32:38 +0200

    (#8) Removed colorAt function from screencapture

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ function bitmap(width, height, byteWidth, bitsPerPixel, bytesPerPixel, image) {
   this.bitsPerPixel = bitsPerPixel;
   this.bytesPerPixel = bytesPerPixel;
   this.image = image;
-
-  this.colorAt = function(x, y) {
-    return libnut.getColor(this, x, y);
-  };
 }
 
 module.exports.screen.highlight = function(x, y, width, height, duration, opacity) {
