2ec2fbe95548c81ef2f8e2b649572a700a272bb5
Simon Hofmann <mail@simon-hofmann.org>
2020-08-06 23:12:05 +0200

    (#17) Moved focus workaround from beforeEach to beforeAll

--- test/window-integration-tests/test.js
+++ test/window-integration-tests/test.js
@@ -15,17 +15,9 @@ beforeAll(async () => {
     });
     await app.start();
     await app.client.waitUntilWindowLoaded();
-});
-
-beforeEach(async () => {
-    if (app && app.isRunning()) {
-        await app.browserWindow.minimize();
-        await app.browserWindow.restore();
-        await app.browserWindow.focus();
-        await app.client.waitUntil(async () => {
-            return await app.browserWindow.isFocused();
-        });
-    }
+    await app.browserWindow.minimize();
+    await app.browserWindow.restore();
+    await app.browserWindow.focus();
 });
 
 describe("getWindows", () => {
