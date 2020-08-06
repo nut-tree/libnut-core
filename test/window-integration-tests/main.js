b0325a807acd2c73d56c0b91e39c979c403303c4
Simon Hofmann <mail@simon-hofmann.org>
2020-08-06 13:38:43 +0200

    (#17) Removed test code inside electron app

--- test/window-integration-tests/main.js
+++ test/window-integration-tests/main.js
@@ -1,18 +1,6 @@
 const {app, ipcMain, BrowserWindow} = require('electron')
-const libnut = require("../..");
 const path = require('path');
-
-const POS_X = 50;
-const POS_Y = 100;
-const WIDTH = 800;
-const HEIGTH = 600;
-
-function test(description, assertion) {
-    console.log(`${description}: ${assertion}`);
-    if (!assertion) {
-        app.exit(1);
-    }
-}
+const { POS_X, POS_Y, WIDTH, HEIGTH } = require("./constants");
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
@@ -25,23 +13,6 @@ function createWindow() {
     });
     mainWindow.loadFile(path.join(__dirname, "index.html"));
     mainWindow.setPosition(POS_X, POS_Y);
-
-    setTimeout(() => {
-        const windowNames = libnut.getWindows().map(
-            handle => libnut.getWindowTitle(handle)
-        );
-        test("list windows", windowNames.includes("libnut window test"));
-
-        const activeWindow = libnut.getActiveWindow();
-        const activeWindowRect = libnut.getWindowRect(activeWindow);
-        const activeWindowTitle = libnut.getWindowTitle(activeWindow);
-        test("title", activeWindowTitle === "libnut window test");
-        test("posX", activeWindowRect.x === POS_X);
-        test("posY", activeWindowRect.y === POS_Y);
-        test("width", activeWindowRect.width === WIDTH);
-        test("height", activeWindowRect.height === HEIGTH);
-        app.quit();
-    }, 5000);
 }
 
 ipcMain.on("main", (event, args) => {
