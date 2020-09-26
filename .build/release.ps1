06139d59c16aaff88ccd55ea62a22f4d9cf9350c
Simon Hofmann <mail@simon-hofmann.org>
2020-09-26 22:28:21 +0200

    (#7) Switched to custom 'deploy_script' for Appveyor since it does not provide a Script deployment provider

--- .build/release.ps1
+++ .build/release.ps1
@@ -1,1 +1,19 @@
-npm publish
+function Pre-Release {
+    $timestamp = Get-Date -Format yyyyMMddhhmmss
+    $patchVersion = (npm --no-git-tag version patch)
+    $nextVersion = "${patchVersion}-next.${timestamp}".Substring(1)
+    echo $nextVersion
+
+    npm version --no-git-tag -f $nextVersion
+    npm run publish:next
+}
+
+function Release {
+    npm publish
+}
+
+if ($env:APPVEYOR_REPO_BRANCH -eq "develop") {
+    Pre-Release
+} else if ($env:APPVEYOR_REPO_TAG -eq "true") {
+    Release
+}
