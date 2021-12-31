36298808be73bed61b48fd2e5d233e3d40b4481c
Simon Hofmann <s1hofmann@users.noreply.github.com>
2021-12-31 12:56:21 +0100

    Feature/91/check requirements (#96)
    * (#91) Added start checks for requirements on macOS and Linux
    
    * (#91) Forgot to wrap the Linux startup code in a proper function
    
    * (#91) Fixed unused variable
    
    * (#91) Fixed include
    
    * (#91) Use XOpenDisplay directly for startup checks
    
    * (#91) Added missing parameter to XOpenDisplay
    
    * (#91) Adjusted test constant to new execution environment on macOS where the default menu bar height increased to 25

---
+++ src/startup.h
@@ -0,0 +1,36 @@
+#include "os.h"
+#include <iostream>
+
+#if defined(IS_MACOSX)
+
+#include <ApplicationServices/ApplicationServices.h>
+
+static void startupCheck(void) __attribute__((constructor));
+static void startupCheck(void)
+{
+	bool isTrustedProcess = AXIsProcessTrusted();
+	if (!isTrustedProcess) {
+		std::cout << "##### WARNING! The application running this script is not a trusted process! Please visit https://github.com/nut-tree/nut.js#macos #####" << std::endl;
+	} 
+}
+
+#elif defined(USE_X11)
+
+	#include "xdisplay.h"
+
+static void startupCheck(void) __attribute__((constructor));
+static void startupCheck(void)
+{
+	Display *display = XOpenDisplay(NULL);
+
+	int32_t majorOpcode;
+	int32_t firstEvent;
+	int32_t firstError;
+	bool isXTestAvailable = XQueryExtension(display, "XTEST", &majorOpcode, &firstEvent, &firstError);
+
+	if (!isXTestAvailable) {
+		std::cout << "##### WARNING! Your system is missing libXtst! Please visit https://github.com/nut-tree/nut.js#linux #####" << std::endl;
+	}
+}
+
+#endif
