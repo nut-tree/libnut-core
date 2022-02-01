2eda180396260499bad12304102bd126a91fc040
Simon Hofmann <s1hofmann@users.noreply.github.com>
2022-02-01 21:34:31 +0100

    Feature/102/missing numpad keys (#104)
    * Added missing mappings
    
    * (#102) Added missing K_NUMPAD_DECIMAL mapping

--- src/main.cc
+++ src/main.cc
@@ -332,6 +332,12 @@ static KeyNames key_names[] =
 	{"numpad_7", K_NUMPAD_7},
 	{"numpad_8", K_NUMPAD_8},
 	{"numpad_9", K_NUMPAD_9},
+	{"numpad_decimal", K_NUMPAD_DECIMAL},
+
+	{"add", K_ADD},
+	{"subtract", K_SUBTRACT},
+	{"multiply", K_MULTIPLY},
+	{"divide", K_DIVIDE},
 
 	{"add", K_ADD},
 	{"subtract", K_SUBTRACT},
