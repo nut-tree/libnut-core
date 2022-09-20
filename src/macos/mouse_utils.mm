5e3022d1b44f35cc886f229c94f46f08d94eef8a
Simon Hofmann <s1hofmann@users.noreply.github.com>
2022-09-20 21:29:54 +0200

    Bugfix/377/window title segfault (#134)
    * (#377) Moved inline functions from header to source file
    
    * (#377) Avoid segfaulting on nil values when fetching window title
    
    * (#377) Add checks and requests for both accessibility and screen sharing (for window titles)
    
    * (#377) Enable arm64 debug builds
    
    * (377) Release references after usage
    
    * (#377) Removed unguarded, macOS specific include since it is not required

--- src/macos/mouse_utils.mm
+++ src/macos/mouse_utils.mm
@@ -5,3 +5,56 @@ double GetDoubleClickTime() {
   double doubleClickInterval = [NSEvent doubleClickInterval];
   return doubleClickInterval * 1000;
 }
+
+bool areSamePoint(MMPoint one, MMPoint other) {
+  return one.x == other.x && one.y == other.y;
+}
+
+void recordClickTime(ClickTimer *timer, bool down,
+                            MMMouseButton button,
+			    MMPoint currentPoint) {
+  clock_t milli = clock();
+  if (button == LEFT_BUTTON) {
+    if (down) {
+      timer->left.down = milli;
+    } else {
+      timer->left.up = milli;
+    }
+  } else if (button == CENTER_BUTTON) {
+    if (down) {
+      timer->middle.down = milli;
+    } else {
+      timer->middle.up = milli;
+    }
+  } else if (button == RIGHT_BUTTON) {
+    if (down) {
+      timer->right.down = milli;
+    } else {
+      timer->right.up = milli;
+    }
+  }
+  timer->clickLocation = currentPoint;
+}
+
+clock_t timeSinceLastClick(ClickTimer *timer, bool down, MMMouseButton button, clock_t currentTime) {
+  if (button == LEFT_BUTTON) {
+    if (down) {
+      return currentTime - timer->left.down;
+    } else {
+      return currentTime - timer->left.up;
+    }
+  } else if (button == CENTER_BUTTON) {
+    if (down) {
+      return currentTime - timer->middle.down;
+    } else {
+      return currentTime - timer->middle.up;
+    }
+  } else if (button == RIGHT_BUTTON) {
+    if (down) {
+      return currentTime - timer->right.down;
+    } else {
+      return currentTime - timer->right.up;
+    }
+  }
+  return 0;
+}
--- src/startup.h
+++ src/startup.h
@@ -8,10 +8,27 @@
 static void startupCheck(void) __attribute__((constructor));
 static void startupCheck(void)
 {
-	bool isTrustedProcess = AXIsProcessTrusted();
+  int showDialog = 1;
+  CFNumberRef showDialogNumber = CFNumberCreate(0, kCFNumberSInt32Type, &showDialog);
+  const void *keys[1] = { kAXTrustedCheckOptionPrompt };
+  const void *values[1] = { showDialogNumber };
+
+  CFDictionaryRef opts = CFDictionaryCreate(NULL, keys, values, 1, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+
+  bool isTrustedProcess = AXIsProcessTrustedWithOptions(opts);
 	if (!isTrustedProcess) {
 		std::cout << "##### WARNING! The application running this script is not a trusted process! Please visit https://github.com/nut-tree/nut.js#macos #####" << std::endl;
 	} 
+
+  bool hasScreenCaptureAccess = CGPreflightScreenCaptureAccess();	
+  if (!hasScreenCaptureAccess) {
+    CGRequestScreenCaptureAccess();
+		std::cout << "##### WARNING! The application running this script is not allowed to capture screen content! Please visit https://github.com/nut-tree/nut.js#macos #####" << std::endl;
+    
+	} 
+
+  CFRelease(showDialogNumber);
+  CFRelease(opts);
 }
 
 #endif
