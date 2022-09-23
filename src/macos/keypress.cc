35c7f7cd53ccd16e8dad24d0cd25f984065b538c
Simon Hofmann <mail@simon-hofmann.org>
2022-09-23 21:36:08 +0200

    (#9) macOS unicode migration: Migrated keypress.c

--- src/macos/keypress.cc
+++ src/macos/keypress.cc
@@ -1,8 +1,8 @@
 #include "../keypress.h"
-#include "../deadbeef_rand.h"
 #include "../microsleep.h"
+#include "../deadbeef_rand.h"
 
-#include <ctype.h> /* For isupper() */
+#include <cctype>
 
 #include <ApplicationServices/ApplicationServices.h>
 #import <IOKit/hidsystem/IOHIDLib.h>
@@ -55,26 +55,27 @@ void toggleKeyCode(MMKeyCode code, const bool down, MMKeyFlags flags) {
 
     // Check if keycode is one of the available modifier keys and set keyflags
     // accordingly
+    // TODO: Add operator overloads for MMKeyFlags
     if (code == K_META) {
-      flags |= MOD_META;
+      flags = static_cast<MMKeyFlags>(flags | MOD_META);
     }
     if (code == K_ALT) {
-      flags |= MOD_ALT;
+      flags = static_cast<MMKeyFlags>(flags | MOD_ALT);
     }
     if (code == K_CONTROL) {
-      flags |= MOD_CONTROL;
+      flags = static_cast<MMKeyFlags>(flags | MOD_CONTROL);
     }
     if (code == K_SHIFT || code == K_RIGHTSHIFT) {
-      flags |= MOD_SHIFT;
+      flags = static_cast<MMKeyFlags>(flags | MOD_SHIFT);
     }
 
     MMKeyFlags activeKeyFlags;
     if (down) {
-      activeKeyFlags = flags | flagBuffer;
-      flagBuffer |= activeKeyFlags;
+      activeKeyFlags = static_cast<MMKeyFlags>(flags | flagBuffer);
+      flagBuffer = static_cast<MMKeyFlags>(flagBuffer | activeKeyFlags);
     } else {
-      activeKeyFlags = flags ^ flagBuffer;
-      flagBuffer ^= flags;
+      activeKeyFlags = static_cast<MMKeyFlags>(flags ^ flagBuffer);
+      flagBuffer = static_cast<MMKeyFlags>(flagBuffer ^ flags);
     }
 
     CGEventSetFlags(keyEvent, activeKeyFlags);
@@ -93,7 +94,7 @@ void toggleKey(char c, const bool down, MMKeyFlags flags) {
   MMKeyCode keyCode = keyCodeForChar(c);
 
   if (isupper(c) && !(flags & MOD_SHIFT)) {
-    flags |= MOD_SHIFT; /* Not sure if this is safe for all layouts. */
+    flags = static_cast<MMKeyFlags>(flags | MOD_SHIFT); /* Not sure if this is safe for all layouts. */
   }
 
   toggleKeyCode(keyCode, down, flags);
@@ -104,87 +105,22 @@ void tapKey(char c, MMKeyFlags flags) {
   toggleKey(c, false, flags);
 }
 
-void toggleUnicodeKey(unsigned long ch, const bool down) {
-  /* This function relies on the convenient
-   * CGEventKeyboardSetUnicodeString(), which allows us to not have to
-   * convert characters to a keycode, but does not support adding modifier
-   * flags. It is therefore only used in typeString() and typeStringDelayed()
-   * -- if you need modifier keys, use the above functions instead. */
+void typeString(const std::u16string &str) {
   CGEventSourceRef src = CGEventSourceCreate(kCGEventSourceStateHIDSystemState);
-  CGEventRef keyEvent = CGEventCreateKeyboardEvent(src, 0, down);
-  if (keyEvent == NULL) {
-    fputs("Could not create keyboard event.\n", stderr);
-    return;
+  for (auto it = str.begin(); it != str.end(); ++it) {
+    UniChar c = *it;
+    CGEventRef downEvent = CGEventCreateKeyboardEvent(src, 0, true);
+    CGEventKeyboardSetUnicodeString(downEvent, 1, &c);
+    CGEventPost(kCGHIDEventTap, downEvent);
+
+    CGEventRef upEvent = CGEventCreateKeyboardEvent(src, 0, false);
+    CGEventKeyboardSetUnicodeString(upEvent, 1, &c);
+    CGEventPost(kCGHIDEventTap, upEvent);
+
+    CFRelease(downEvent);
+    CFRelease(upEvent);
+		microsleep(DEADBEEF_RANDRANGE(1, 10));
   }
-
-  if (ch > 0xFFFF) {
-    // encode to utf-16 if necessary
-    unsigned short surrogates[] = {0xD800 + ((ch - 0x10000) >> 10),
-                                   0xDC00 + (ch & 0x3FF)};
-
-    CGEventKeyboardSetUnicodeString(keyEvent, 2, &surrogates);
-  } else {
-    CGEventKeyboardSetUnicodeString(keyEvent, 1, &ch);
-  }
-
-  CGEventPost(kCGHIDEventTap, keyEvent);
-  CFRelease(keyEvent);
   CFRelease(src);
 }
 
-void toggleUniKey(char c, const bool down) { toggleUnicodeKey(c, down); }
-
-static void tapUniKey(char c) {
-  toggleUniKey(c, true);
-  toggleUniKey(c, false);
-}
-
-void typeString(const char *str) {
-  unsigned short c;
-  unsigned short c1;
-  unsigned short c2;
-  unsigned short c3;
-  unsigned long n;
-
-  while (*str != '\0') {
-    c = *str++;
-
-    // warning, the following utf8 decoder
-    // doesn't perform validation
-    if (c <= 0x7F) {
-      // 0xxxxxxx one byte
-      n = c;
-    } else if ((c & 0xE0) == 0xC0) {
-      // 110xxxxx two bytes
-      c1 = (*str++) & 0x3F;
-      n = ((c & 0x1F) << 6) | c1;
-    } else if ((c & 0xF0) == 0xE0) {
-      // 1110xxxx three bytes
-      c1 = (*str++) & 0x3F;
-      c2 = (*str++) & 0x3F;
-      n = ((c & 0x0F) << 12) | (c1 << 6) | c2;
-    } else if ((c & 0xF8) == 0xF0) {
-      // 11110xxx four bytes
-      c1 = (*str++) & 0x3F;
-      c2 = (*str++) & 0x3F;
-      c3 = (*str++) & 0x3F;
-      n = ((c & 0x07) << 18) | (c1 << 12) | (c2 << 6) | c3;
-    }
-
-    toggleUnicodeKey(n, true);
-    toggleUnicodeKey(n, false);
-  }
-}
-
-void typeStringDelayed(const char *str, const unsigned cpm) {
-  /* Characters per second */
-  const double cps = (double)cpm / 60.0;
-
-  /* Average milli-seconds per character */
-  const double mspc = (cps == 0.0) ? 0.0 : 1000.0 / cps;
-
-  while (*str != '\0') {
-    tapUniKey(*str++);
-    microsleep(mspc + (DEADBEEF_UNIFORM(0.0, 62.5)));
-  }
-}
