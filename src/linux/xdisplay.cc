120333e1eb12bc3df6dcc0298e44badb96800407
Simon Hofmann <mail@simon-hofmann.org>
2022-09-26 13:51:09 +0200

    (#9) Switched to using libxdo for typeString

--- src/linux/xdisplay.cc
+++ src/linux/xdisplay.cc
@@ -1,5 +1,6 @@
 #include "../xdisplay.h"
-#include <stdio.h> /* For fputs() */
+#include "libxdo/xdo.h"
+#include <stdio.h>  /* For fputs() */
 #include <stdlib.h> /* For atexit() */
 #include <string.h> /* For strdup() */
 
@@ -8,21 +9,28 @@ static int registered = 0;
 static char *displayName = NULL;
 static int hasDisplayNameChanged = 0;
 
+static xdo_t *xdo = NULL;
+
 Display *XGetMainDisplay(void)
 {
 	/* Close the display if displayName has changed */
-	if (hasDisplayNameChanged) {
+	if (hasDisplayNameChanged)
+	{
 		XCloseMainDisplay();
 		hasDisplayNameChanged = 0;
 	}
 
-	if (mainDisplay == NULL) {
+	if (mainDisplay == NULL)
+	{
 		/* First try the user set displayName */
 		mainDisplay = XOpenDisplay(displayName);
 
-		if (mainDisplay == NULL) {
+		if (mainDisplay == NULL)
+		{
 			fputs("Could not open main display\n", stderr);
-		} else if (!registered) {
+		}
+		else if (!registered)
+		{
 			atexit(&XCloseMainDisplay);
 			registered = 1;
 		}
@@ -31,15 +39,38 @@ Display *XGetMainDisplay(void)
 	return mainDisplay;
 }
 
+xdo_t *get_xdo()
+{
+	if (xdo == NULL)
+	{
+		xdo = xdo_new_with_opened_display(XGetMainDisplay(), displayName, false);
+	}
+	else if (!registered)
+	{
+		atexit(&free_xdo);
+		registered = 1;
+	}
+	return xdo;
+}
+
+void free_xdo()
+{
+	if (xdo != NULL)
+	{
+		xdo_free(xdo);
+	}
+}
+
 void XCloseMainDisplay(void)
 {
-	if (mainDisplay != NULL) {
+	if (mainDisplay != NULL)
+	{
 		XCloseDisplay(mainDisplay);
 		mainDisplay = NULL;
 	}
 }
 
-char * getXDisplay(void)
+char *getXDisplay(void)
 {
 	return displayName;
 }
