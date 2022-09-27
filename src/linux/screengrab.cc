bbf1a569cc0f584648537fed44dd05d73d8bcace
Simon Hofmann <mail@simon-hofmann.org>
2022-09-27 23:24:28 +0200

    (#9) Migrated Linux screengrab implementation

--- src/linux/screengrab.cc
+++ src/linux/screengrab.cc
@@ -1,15 +1,12 @@
 #include "../screengrab.h"
 #include "../endian.h"
-#include <stdlib.h> /* malloc() */
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include "../xdisplay.h"
 
-MMBitmapRef copyMMBitmapFromDisplayInRect(MMRect rect)
+std::shared_ptr<Bitmap> copyMMBitmapFromDisplayInRect(MMRect rect)
 {
-	MMBitmapRef bitmap;
-
 	Display *display = XOpenDisplay(NULL);
 	XImage *image = XGetImage(display,
 	                          XDefaultRootWindow(display),
@@ -21,12 +18,11 @@ MMBitmapRef copyMMBitmapFromDisplayInRect(MMRect rect)
 	XCloseDisplay(display);
 	if (image == NULL) return NULL;
 
-	bitmap = createMMBitmap((uint8_t *)image->data,
+	std::shared_ptr<Bitmap> bitmap(new Bitmap((uint8_t *)image->data,
 	                        rect.size.width,
 	                        rect.size.height,
-	                        (size_t)image->bytes_per_line,
-	                        (uint8_t)image->bits_per_pixel,
-	                        (uint8_t)image->bits_per_pixel / 8);
+	                        image->bytes_per_line,
+	                        image->bits_per_pixel));
 	image->data = NULL; /* Steal ownership of bitmap data so we don't have to
 	                     * copy it. */
 	XDestroyImage(image);
